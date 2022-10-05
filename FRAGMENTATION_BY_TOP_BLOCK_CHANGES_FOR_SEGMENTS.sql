64bec1236058f60194f3741cb806df59c94abbe2
Zainuddin Syed <101149770+zsyed630@users.noreply.github.com>
2022-10-05 10:06:02 -0500

    Update FRAGMENTATION_BY_TOP_BLOCK_CHANGES_FOR_SEGMENTS.sql

--- FRAGMENTATION_BY_TOP_BLOCK_CHANGES_FOR_SEGMENTS.sql
+++ FRAGMENTATION_BY_TOP_BLOCK_CHANGES_FOR_SEGMENTS.sql
@@ -7,8 +7,8 @@ set serverout on
 set head off
 set verify off
 
-define STARTTIME='2022-09-06  07:00:00';
-define ENDTIME='2022-09-07  19:00:00';
+define STARTTIME='2022-09-12  07:00:00';
+define ENDTIME='2022-09-19  19:00:00';
 
 
 DECLARE
@@ -52,22 +52,34 @@ BEGIN
             IF v_unq_object_names.object_type like '%PARTITION%'
             THEN
 
-                select round(bytes) into OBJECT_SIZE_BYTES from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
-                select round(bytes/1024/1024/1024) into OBJECT_SIZE_GB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
-                select round(bytes/1024/1024) into OBJECT_SIZE_MB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
-                select round(bytes/1024) into OBJECT_SIZE_KB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
-                select round(bytes) into OBJECT_SIZE_BYTES from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
+                select round(bytes) into OBJECT_SIZE_BYTES from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name and segment_type = v_unq_object_names.object_type;
+                select round(bytes/1024/1024/1024) into OBJECT_SIZE_GB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name and segment_type = v_unq_object_names.object_type;
+                select round(bytes/1024/1024) into OBJECT_SIZE_MB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name and segment_type = v_unq_object_names.object_type;
+                select round(bytes/1024) into OBJECT_SIZE_KB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name and segment_type = v_unq_object_names.object_type;
+                select round(bytes) into OBJECT_SIZE_BYTES from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name and segment_type = v_unq_object_names.object_type;
             
-                IF v_unq_object_names.object_type like '%INDEX%'
+                IF v_unq_object_names.object_type = 'INDEX PARTITION'
                 THEN
-                    select PCT_FREE into V_PCT_FREE from dba_ind_partitions where index_owner = v_unq_object_names.owner and partition_name = v_unq_object_names.subobject_name;
-                    select INI_TRANS into V_INI_TRANS from dba_ind_partitions where index_owner = v_unq_object_names.owner and partition_name = v_unq_object_names.subobject_name;
-                    select LAST_ANALYZED into V_LAST_ANALYZED from dba_ind_partitions where index_owner = v_unq_object_names.owner and partition_name = v_unq_object_names.subobject_name;
-                ELSE
-                    select PCT_FREE into V_PCT_FREE from dba_tab_partitions where table_owner = v_unq_object_names.owner and partition_name = v_unq_object_names.subobject_name;
-                    select PCT_USED into V_PCT_USED from dba_tab_partitions where table_owner = v_unq_object_names.owner and partition_name = v_unq_object_names.subobject_name;
-                    select INI_TRANS into V_INI_TRANS from dba_tab_partitions where table_owner = v_unq_object_names.owner and partition_name = v_unq_object_names.subobject_name;
-                    select LAST_ANALYZED into V_LAST_ANALYZED from dba_tab_partitions where table_owner = v_unq_object_names.owner and partition_name = v_unq_object_names.subobject_name;
+                    select PCT_FREE into V_PCT_FREE from dba_ind_partitions where index_owner = v_unq_object_names.owner and index_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name ;
+                    select INI_TRANS into V_INI_TRANS from dba_ind_partitions where index_owner = v_unq_object_names.owner and index_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
+                    select LAST_ANALYZED into V_LAST_ANALYZED from dba_ind_partitions where index_owner = v_unq_object_names.owner and index_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
+                ELSIF v_unq_object_names.object_type = 'INDEX SUBPARTITION'
+                THEN
+                    select PCT_FREE into V_PCT_FREE from dba_ind_subpartitions where index_owner = v_unq_object_names.owner and index_name = v_unq_object_names.object_name and subpartition_name = v_unq_object_names.subobject_name ;
+                    select INI_TRANS into V_INI_TRANS from dba_ind_subpartitions where index_owner = v_unq_object_names.owner and index_name = v_unq_object_names.object_name and subpartition_name = v_unq_object_names.subobject_name;
+                    select LAST_ANALYZED into V_LAST_ANALYZED from dba_ind_subpartitions where index_owner = v_unq_object_names.owner and index_name = v_unq_object_names.object_name and subpartition_name = v_unq_object_names.subobject_name;
+                ELSIF v_unq_object_names.object_type = 'TABLE PARTITION'
+                THEN
+                    select PCT_FREE into V_PCT_FREE from dba_tab_partitions where table_owner = v_unq_object_names.owner and table_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
+                    select PCT_USED into V_PCT_USED from dba_tab_partitions where table_owner = v_unq_object_names.owner and table_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
+                    select INI_TRANS into V_INI_TRANS from dba_tab_partitions where table_owner = v_unq_object_names.owner and table_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
+                    select LAST_ANALYZED into V_LAST_ANALYZED from dba_tab_partitions where table_owner = v_unq_object_names.owner and table_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
+                ELSIF v_unq_object_names.object_type = 'TABLE SUBPARTITION'
+                THEN
+                    select PCT_FREE into V_PCT_FREE from dba_tab_subpartitions where table_owner = v_unq_object_names.owner and table_name = v_unq_object_names.object_name and subpartition_name = v_unq_object_names.subobject_name;
+                    select PCT_USED into V_PCT_USED from dba_tab_subpartitions where table_owner = v_unq_object_names.owner and table_name = v_unq_object_names.object_name and subpartition_name = v_unq_object_names.subobject_name;
+                    select INI_TRANS into V_INI_TRANS from dba_tab_subpartitions where table_owner = v_unq_object_names.owner and table_name = v_unq_object_names.object_name and subpartition_name = v_unq_object_names.subobject_name;
+                    select LAST_ANALYZED into V_LAST_ANALYZED from dba_tab_subpartitions where table_owner = v_unq_object_names.owner and table_name = v_unq_object_names.object_name and subpartition_name = v_unq_object_names.subobject_name;
                 END IF;
 
 
@@ -104,11 +116,11 @@ BEGIN
                 full_blocks     => v_full_blocks,
                 unformatted_blocks => v_unformatted_blocks,
                 unformatted_bytes => v_unformatted_bytes);
-                select round(bytes) into OBJECT_SIZE_BYTES from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
-                select round(bytes/1024/1024/1024) into OBJECT_SIZE_GB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
-                select round(bytes/1024/1024) into OBJECT_SIZE_MB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
-                select round(bytes/1024) into OBJECT_SIZE_KB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
-                select round(bytes) into OBJECT_SIZE_BYTES from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name;
+                select round(bytes) into OBJECT_SIZE_BYTES from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name and segment_type = v_unq_object_names.object_type;
+                select round(bytes/1024/1024/1024) into OBJECT_SIZE_GB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name and segment_type = v_unq_object_names.object_type;
+                select round(bytes/1024/1024) into OBJECT_SIZE_MB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name and segment_type = v_unq_object_names.object_type;
+                select round(bytes/1024) into OBJECT_SIZE_KB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name and segment_type = v_unq_object_names.object_type; 
+                select round(bytes) into OBJECT_SIZE_BYTES from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and partition_name = v_unq_object_names.subobject_name and segment_type = v_unq_object_names.object_type;
                 select round(((v_freespace1_bytes+v_freespace2_bytes+v_freespace3_bytes+v_freespace4_bytes)/OBJECT_SIZE_BYTES)*100) into fragmented_percentage from dual;
 
 
@@ -126,6 +138,10 @@ BEGIN
                 dbms_output.put_line('PARTITION_NAME : '||v_unq_object_names.subobject_name);
                 dbms_output.put_line('PARTITION_SIZE : '||OBJECT_SIZE_GB||'GB '||OBJECT_SIZE_MB||'MB '||OBJECT_SIZE_KB||'KB ');
                 dbms_output.put_line('FRAGMENTED : YES, '||fragmented_percentage||'%');
+                dbms_output.put_line('PCT_FREE : '||V_PCT_FREE);
+                dbms_output.put_line('PCT_USED : '||V_PCT_USED);
+                dbms_output.put_line('INI_TRANS : '||V_INI_TRANS);
+                dbms_output.put_line('LAST_ANALYZED : '||V_LAST_ANALYZED);
                 dbms_output.put_line('TIME PERIOD : '||DEFINED_STARTTIME||' TO '||DEFINED_ENDTIME);
                 dbms_output.put_line('Blocks with Free Space (0-25%)  = '||v_freespace1_blocks);
                 dbms_output.put_line('Blocks with Free Space (25-50%) = '||v_freespace2_blocks);
@@ -147,6 +163,10 @@ BEGIN
                 dbms_output.put_line('PARTITION_NAME : '||v_unq_object_names.subobject_name);
                 dbms_output.put_line('PARTITION_SIZE : '||OBJECT_SIZE_GB||'GB '||OBJECT_SIZE_MB||'MB '||OBJECT_SIZE_KB||'KB ');
                 dbms_output.put_line('FRAGMENTED : NO, '||fragmented_percentage||'%');
+                dbms_output.put_line('PCT_FREE : '||V_PCT_FREE);
+                dbms_output.put_line('PCT_USED : '||V_PCT_USED);
+                dbms_output.put_line('INI_TRANS : '||V_INI_TRANS);
+                dbms_output.put_line('LAST_ANALYZED : '||V_LAST_ANALYZED);
                 dbms_output.put_line('TIME PERIOD : '||DEFINED_STARTTIME||' TO '||DEFINED_ENDTIME);
                 dbms_output.put_line('Blocks with Free Space (0-25%)  = '||v_freespace1_blocks);
                 dbms_output.put_line('Blocks with Free Space (25-50%) = '||v_freespace2_blocks);
@@ -160,6 +180,18 @@ BEGIN
 
             ELSE
 
+                IF v_unq_object_names.object_type = 'TABLE'
+                THEN
+                    select PCT_FREE into V_PCT_FREE from dba_tables where owner = v_unq_object_names.owner and table_name = v_unq_object_names.object_name;
+                    select PCT_USED into V_PCT_USED from dba_tables where owner = v_unq_object_names.owner and table_name = v_unq_object_names.object_name;
+                    select INI_TRANS into V_INI_TRANS from dba_tables where owner = v_unq_object_names.owner and table_name = v_unq_object_names.object_name;
+                    select LAST_ANALYZED into V_LAST_ANALYZED from dba_tables where owner = v_unq_object_names.owner and table_name = v_unq_object_names.object_name;
+                ELSIF v_unq_object_names.object_type = 'INDEX'
+                THEN
+                    select PCT_FREE into V_PCT_FREE from dba_indexes where owner = v_unq_object_names.owner and index_name = v_unq_object_names.object_name;
+                    select INI_TRANS into V_INI_TRANS from dba_indexes where owner = v_unq_object_names.owner and index_name = v_unq_object_names.object_name;
+                    select LAST_ANALYZED into V_LAST_ANALYZED from dba_indexes where owner = v_unq_object_names.owner and index_name = v_unq_object_names.object_name;
+                END IF;
 
                 declare
                 v_freespace1_bytes number;
@@ -193,12 +225,12 @@ BEGIN
                 full_blocks     => v_full_blocks,
                 unformatted_blocks => v_unformatted_blocks,
                 unformatted_bytes => v_unformatted_bytes);
-                select round(bytes) into OBJECT_SIZE_BYTES from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name ;
+                select round(bytes) into OBJECT_SIZE_BYTES from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and segment_type = v_unq_object_names.object_type ;
                 select round(((v_freespace1_bytes+v_freespace2_bytes+v_freespace3_bytes+v_freespace4_bytes)/OBJECT_SIZE_BYTES)*100) into fragmented_percentage from dual;
-                select round(bytes/1024/1024/1024) into OBJECT_SIZE_GB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name;
-                select round(bytes/1024/1024) into OBJECT_SIZE_MB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name;
-                select round(bytes/1024) into OBJECT_SIZE_KB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name;
-                select round(bytes) into OBJECT_SIZE_BYTES from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name;
+                select round(bytes/1024/1024/1024) into OBJECT_SIZE_GB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and segment_type = v_unq_object_names.object_type;
+                select round(bytes/1024/1024) into OBJECT_SIZE_MB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and segment_type = v_unq_object_names.object_type;
+                select round(bytes/1024) into OBJECT_SIZE_KB from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and segment_type = v_unq_object_names.object_type;
+                select round(bytes) into OBJECT_SIZE_BYTES from dba_segments where owner = v_unq_object_names.owner and segment_name = v_unq_object_names.object_name and segment_type = v_unq_object_names.object_type;
                 
                 IF fragmented_percentage > 20 
                 THEN
@@ -213,6 +245,10 @@ BEGIN
                 dbms_output.put_line('OBJECT_NAME : '||v_unq_object_names.object_name);
                 dbms_output.put_line('OBJECT_TYPE : '||v_unq_object_names.object_type);
                 dbms_output.put_line('FRAGMENTED : YES, '||fragmented_percentage||'%');
+                dbms_output.put_line('PCT_FREE : '||V_PCT_FREE);
+                dbms_output.put_line('PCT_USED : '||V_PCT_USED);
+                dbms_output.put_line('INI_TRANS : '||V_INI_TRANS);
+                dbms_output.put_line('LAST_ANALYZED : '||V_LAST_ANALYZED);
                 dbms_output.put_line('TIME PERIOD : '||DEFINED_STARTTIME||' TO '||DEFINED_ENDTIME);
                 dbms_output.put_line('Blocks with Free Space (0-25%)  = '||v_freespace1_blocks);
                 dbms_output.put_line('Blocks with Free Space (25-50%) = '||v_freespace2_blocks);
@@ -232,6 +268,10 @@ BEGIN
                 dbms_output.put_line('OBJECT_NAME : '||v_unq_object_names.object_name);
                 dbms_output.put_line('OBJECT_TYPE : '||v_unq_object_names.object_type);
                 dbms_output.put_line('FRAGMENTED : NO, '||fragmented_percentage||'%');
+                dbms_output.put_line('PCT_FREE : '||V_PCT_FREE);
+                dbms_output.put_line('PCT_USED : '||V_PCT_USED);
+                dbms_output.put_line('INI_TRANS : '||V_INI_TRANS);
+                dbms_output.put_line('LAST_ANALYZED : '||V_LAST_ANALYZED);
                 dbms_output.put_line('TIME PERIOD : '||DEFINED_STARTTIME||' TO '||DEFINED_ENDTIME);
                 dbms_output.put_line('Blocks with Free Space (0-25%)  = '||v_freespace1_blocks);
                 dbms_output.put_line('Blocks with Free Space (25-50%) = '||v_freespace2_blocks);
@@ -246,7 +286,6 @@ BEGIN
         END LOOP;
 END;
 /
-
 
 
 
